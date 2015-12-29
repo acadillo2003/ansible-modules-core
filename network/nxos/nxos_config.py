38c96d2dcc028143c5dcfed53b1270de4321c100
Peter Sprygada <psprygada@ansible.com>
2015-12-29 11:25:47 -0500

    refactors nxos_config module to use parser and template engine

--- network/nxos/nxos_config.py
+++ network/nxos/nxos_config.py
@@ -15,380 +15,95 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 #
-
 DOCUMENTATION = """
----
-module: nxos_config
-version_added: "2.0"
-author: "Peter Sprygada (@privateip)"
-short_description: Send and receive NXOS configuration commands over NXAPI
-description:
-  - Sends and receives NXOS configuration commands using NXAPI over a
-    HTTP/S transport.  This module supports sending either a single
-    command or an ordered set of the remote host.  This module
-    requires that NXAPI support has been enabled on the device.
-notes:
-  - Supports additional arguments via the NXAPI shared modules.  See
-    module_utils/nxapi.py for details.
-options:
-  line:
-    description:
-      - The configuration line in the device configuration that should be
-        present  The current node configuration is evaluated and if
-        the line is not found it will be added
-    required: false
-    default: null
-  replace:
-    description:
-      - The value for the replace argument will be inserted into the
-        current nodes running configuraiton if the value of C(line) is
-        not found.  The C(replace) argument is mutually exclusive with the
-        C(block) argument
-    required: false
-    default: null
-  block:
-    description:
-      - The block of configuration to apply to the node.  Use block
-        to configure more than one line statement.  For each line
-        in the block, the configuration is evaluated and missing lines
-        are added to the configuration based on the C(strategy).  The
-        C(block) and C(line) arguments are mutually exclusive.
-    type: list
-    required: false
-    default: null
-  parent:
-    description:
-      - The configuration parent for the C(line) or C(block).  The
-        C(parent) argument is a one line configuration statement
-        for entering sub mode configuration commands.  It is mutually
-        exclusive with C(ancestors).  See EXAMPLES for details.
-    required: false
-    default: null
-  ancestors:
-    description:
-      - The ancestors of the block or line of configuration.  The
-        C(ancestors) argument allows for nested configuration options and
-        is mutually exclusive with C(parent).  See EXAMPLES for details.
-    type: list
-    required: false
-    default: null
-  before_block:
-    description:
-      - A set of configuration commands to execute before the block.  The
-        C(before_block) is execute if and only if the commands in either
-        C(line) or C(block) trigger a configuration change.
-    required: false
-    default: null
-  after_block:
-    description:
-      - a set of configuration commands to execute after the block.  This
-        argument is the same as C(before_block) only it is configured
-        once the C(block) statements are completed
-    required: false
-    default: null
-  match:
-    description:
-      - Specifies the match strategy to use when evaluting a block of
-        configuration from the node.  By default, the match strategy is
-        configured to evaluate a line and if the line isn't present it
-        will be added to the config.  If the match strategy is set to
-        block, the entire block is added to the configuration if any line
-        is invalid.  If the match strategy is set of exact, the config
-        block must match exactly. Finally setting the value to C(force)
-        will not evalue the current config and simply configure the node
-        with the block.
-    required: false
-    default: line
-    choices: ['line', 'block', 'exact', 'force]
-  config:
-    description:
-      - Overrides the device configuration file with this value.  By
-        default, this module will attach to the node to collect the current
-        running-config.  Using this argument will override that behavior
-        and validate the configuration passed
-    required: false
-    default: null
-  config_file:
-    description:
-      - The path to the device configuration for offline access to the
-        configuration file.  This option is the same as the C(config)
-        option only it will read the config contents from a file.  The
-        C(config) and C(config_file) arguments are mutually exclusive
-    required: false
-    default: null
-  offline:
-    description:
-      - This flag controls if the module runs in online or offline mode.  When
-        in online mode, the module will connect to the device to get the
-        config and push config changes.  When in offline mode, the module
-        expects the config or config_file argument to provided and it
-        generates a list of commands in the result
-    required: false
-    default: false
 """
 
 EXAMPLES = """
-# Note: These examples do not set nxapi parameters, see module_utils/nxapi.py
-
-# make a simple configuration change
-- nxos_config:
-    line: "hostname switch"
-
-# configure an interface block
-- nxos_config:
-    block:
-      - "description example config block"
-      - "ip address 1.1.1.1/32"
-      - "no shutdown"
-    parent: "interface loopback0"
-
-# configure an access list.  this example will evalue the current
-# configuration and if any statement doesn't match in the acl
-# it will remove the acl (the before_block)  and reconfigured
-# it with statements in block
-- nxos_config:
-    block:
-      - "10 permit ip 172.30.10.0/24 any"
-      - "20 permit ip 172.30.11.0/24 any"
-      - "30 permit ip 172.30.12.0/24 any"
-      - "40 permit ip 172.30.13.0/24 any"
-    parent: "ip access-list example"
-    before_block:
-      - "no ip access-list example"
-    strategy: all
-
-# use ancestors to configure nested commands
-- nxos_config:
-    block:
-      - "remote-as 65000"
-      - "update source loopback0"
-    ancestors: ["router bgp 65000", "neighbor 1.1.1.1"]
-
 """
 
 RETURN = """
-
-commands:
-  description: The list of commands used to configure the device
-  returned: success
-  type: list
-  sample: "[{ ... }]"
-
-config:
-  description: The block extracted from the running-config
-  returned: success
-  type: list
-  sample: "[...]"
-
 """
-import re
-import collections
 
-def parse(config, indent=2):
-    regexp = re.compile(r'^\s*(.+)$')
-    line_re = re.compile(r'\S')
-    invalid_re = re.compile(r'^(!|end)')
-
-    ancestors = list()
-    data = collections.OrderedDict()
-    banner = False
-
-    for line in config:
-        text = str(line).strip()
-
-        # TODO: add support for parsing banners
-        if not invalid_re.match(text):
-
-            # handle top level commands
-            if line_re.match(line):
-                data[text] = collections.OrderedDict()
-                ancestors = [text]
-
-            # handle sub level commands
-            else:
-                match = regexp.match(line)
-                if match:
-                    line_indent = match.start(1)
-                    level = line_indent / indent
-
-                    try:
-                        ancestors[level] = text
-                    except IndexError:
-                        ancestors.append(text)
-
-                    try:
-                        child = data[ancestors[0]]
-                        for a in ancestors[1:level]:
-                            child = child[a]
-                        child[text] = collections.OrderedDict()
-                    except KeyError:
-                        # FIXME deal with indent inconsistencies
-                        if '_errors' not in data:
-                            data['_errors'] = list()
-                        data['_errors'].append((ancestors, text))
-
-    return data
-
-def to_list(arg):
-    if isinstance(arg, (list, tuple)):
-        return list(arg)
-    elif arg is not None:
-        return [arg]
-    else:
-        return []
-
-def get_config(module):
-    config = module.params['config']
-    if not config:
+def get_config(conn, module):
+    config = module.params['config'] or dict()
+    if not config and not module.params['force']:
         cmd = 'show running-config'
-        if  module.params['include_all']:
+        if module.params['include_all']:
             cmd += ' all'
-        config = nxapi_command(module, cmd)
-        config = config[0]['ins_api']['outputs']['output']['body']
-    return config or dict()
-
-def load_config(module):
-    try:
-        filename = module.params['config_file']
-        return open(filename).read()
-    except IOError, exc:
-        return module.fail_json(msg=exc.strerror, errno=exc.errno)
-
-def parse_config(config, ancestors=None):
-    config = parse(str(config).split('\n'))
-    try:
-        for ancestor in ancestors:
-            config = config[ancestor]
-    except KeyError:
-        return dict()
-    except TypeError:
-        pass
+        resp = conn.send(cmd)
+        config = resp[0]
     return config
 
-def match_re(pattern, values):
-    regexp = re.compile(r'%s' % pattern)
-    for value in values:
-        match = regexp.search(value)
-        if match:
-            return match
+def backup_config(config, module):
+    host = module.params['cli_host']
+    open('backup_%s' % host, 'w').write(config)
 
-def apply_positional(value, match):
-    if match is None:
-        return value
-    # TODO: trap IndexError
-    for v in re.findall(r"\\\d+", value):
-        index = int(v.replace('\\', ''))
-        val = match.group(index)
-        value = value.replace(v, val)
-    return value
+def on_connect(conn, module):
+    conn.send('terminal length 0')
+    if module.params['cli_authorize']:
+        conn.authorize(module.params['cli_auth_pass'])
 
-def apply_named(values, matches):
-    for value in values:
-        for match in matches:
-            if match:
-                value = value.format(**match.groupdict())
-        yield value
-
+def on_configure(conn):
+    conn.send('configure terminal')
 
 def main():
 
-    spec = dict(
-        line=dict(),
-        replace=dict(),
-        block=dict(type='list'),
-        block_replace=dict(type='list'),
-        parent=dict(),
-        ancestors=dict(type='list'),
-        before_block=dict(type='list'),
-        after_block=dict(type='list'),
-        match=dict(default='line', choices=['line', 'block', 'force']),
-        config=dict(),
-        config_file=dict(),
+    argument_spec = dict(
+        src=dict(),
+        backup=dict(default=False, type='bool'),
+        force=dict(default=False, type='bool'),
+        replace=dict(default=False, type='bool'),
         include_all=dict(default=True, type='bool'),
-        offline=dict(default=False, type='bool'),
-        state=dict(default='present', choices=['present', 'absent'])
+        config=dict()
     )
 
-    required_one_of = [('line', 'block')]
-    mutually_exclusive = [('parent', 'ancestors'), ('config', 'config_file'),
-                          ('block', 'replace'), ('line', 'block')]
+    mutually_exclusive = [('config', 'backup'), ('config', 'force')]
 
-    module = nxapi_module(argument_spec=spec,
-                          mutually_exclusive=mutually_exclusive,
-                          required_one_of=required_one_of,
-                          supports_check_mode=True)
+    module = cli_module(argument_spec=argument_spec,
+                        mutually_exclusive=mutually_exclusive,
+                        supports_check_mode=True)
 
-    offline = module.params['offline']
-    if offline:
-        if not module.params['config'] and not module.params['config_file']:
-            module.fail_json(msg='config or config_file must be specified '
-                                 'when offline is enabled')
-    else:
-        if not module.params['host'] and not module.params['device']:
-            module.fail_json(msg='host or device argument must be specified '
-                                 'when using module in online mode')
+    connection = cli_connection(module)
 
-    before_block = module.params['before_block']
-    after_block = module.params['after_block']
-
-    line = module.params['line']
-    block = module.params['block'] or [line]
-
+    src = module.params['src']
+    force = module.params['force']
+    backup = module.params['backup']
     replace = module.params['replace']
 
-    parent = module.params['parent']
-    ancestors = module.params['ancestors']
-    if not ancestors and parent:
-        ancestors = [parent]
+    candidate = parse(src, indent=2)
 
-    replace = module.params['replace']
-    block_replace = module.params['block_replace']
-    if not block_replace and replace:
-        block_replace = [replace]
-    elif not block_replace:
-        block_replace = list(block)
+    contents = get_config(connection, module)
+    config = parse(contents, indent=2)
 
-    parent = module.params['parent']
-    ancestors = module.params['ancestors']
-    if not ancestors and parent:
-        ancestors = [parent]
-
-    if module.params['config_file']:
-        contents = load_config(module)
-    else:
-        contents = get_config(module)
-    config = parse_config(contents, ancestors)
-    config = config.keys()
+    if backup and not module.check_mode:
+        backup_config(contents, module)
 
     result = dict(changed=False)
-    commands = list()
-    matches = list()
 
-    for cmd, replace in zip(block, block_replace):
-        match = match_re(cmd, config)
-        matches.append(match)
-        if (not match and module.params['state'] == 'present') or \
-           (match and module.params['state'] == 'absent') or \
-           (module.params['match'] == 'force'):
-            commands.append(apply_positional(replace, match))
+    commands = collections.OrderedDict()
+    toplevel = [c.text for c in config]
+
+    for line in candidate:
+        if line.text in ['!', '']:
+            continue
+
+        if not line.parents:
+            if line.text not in toplevel:
+                expand(line, commands)
+        else:
+            item = compare(line, config)
+            if item:
+                expand(item, commands)
+
+    commands = flatten(commands, list())
 
     if commands:
-        if ancestors:
-            commands[:0] = ancestors
-
-        if before_block:
-            commands[:0] = before_block
-
-        if after_block:
-            commands.extend(after_block)
-
-        if not module.check_mode and not offline:
-            response = nxapi_command(module, commands, 'cli_conf')
-            output = to_list(response[0]['ins_api']['outputs']['output'])
-            for resp in output:
-                if int(resp['code']) != 200:
-                    module.fail_json(**resp)
-
+        if not module.check_mode:
+            try:
+                commands = [str(c).strip() for c in commands]
+                response = connection.configure(commands)
+            except ShellError, exc:
+                return module.fail_json(msg=exc.message, command=exc.command)
         result['changed'] = True
 
     result['commands'] = commands
@@ -396,8 +111,9 @@ def main():
 
 
 from ansible.module_utils.basic import *
-from ansible.module_utils.urls import *
-from ansible.module_utils.nxapi import *
-
+from ansible.module_utils.issh import *
+from ansible.module_utils.cli import *
+from ansible.module_utils.net_config import *
 if __name__ == '__main__':
     main()
+
