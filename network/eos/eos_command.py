c018d00aad4e5d50597deffabd0a1bf4f8df59c8
Peter Sprygada <psprygada@ansible.com>
2016-01-19 19:39:33 -0500

    add eos_command module updates

--- network/eos/eos_command.py
+++ network/eos/eos_command.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+#
 # This file is part of Ansible
 #
 # Ansible is free software: you can redistribute it and/or modify
@@ -17,98 +17,126 @@
 #
 
 DOCUMENTATION = """
----
-module: eos_command
-version_added: "2.0"
-author: "Peter Sprygada (@privateip)"
-short_description: Send and receive EOS commands over eAPI to Arista devices
-description:
-  - Sends and receives EOS commands using eAPI over a HTTP/S transport.  This
-    module supports sending either a single command or an ordered set of
-    the remote host.  This module requires that eAPI support has been enabled
-    on the device.
-notes:
-  - Supports shared module arguments via the EAPI shared module.  See
-    module_utils/eapi.py for details
-  - EOS 4.14 or later
-options:
-  command:
-    description:
-      - Sends a single command to the Arista EOS node over eAPI and returns
-        the response.
-    required: false
-    default: null
-  commands:
-    description:
-      - Sends an ordered set of commadns to the EOS node over eAPI.  Commands
-        are batch sent.  If any one command fails, the module will stop
-        executing any futher commands.
-    required: false
-    default: null
-  encoding:
-    description:
-      - Specifies the desired encoding for the response.  Responses from
-        commands can be encoded as either C(json) or C(text).  Note: some
-        EOS commands do not support JSON encoding.  All commands will be
-        encoded using the same method
-    required: false
-    default: json
-    choices: ['json', 'text']
-
 """
 
 EXAMPLES = """
-
-# Note: These examples do not set eapi parameters, see module_utils/eapi.py
-
-# Execute the show version command on the remote host
-- eos_command: command='show version'
-
-# Execute multiple commands and return as text
-- eos_command:
-    commands:
-      - show version
-      - show ip route
-    encoding: text
-
 """
 
 RETURN = """
-
-result:
-  description: the output from the command run in in desired encoding
-  returned: success
-  type: list
-  sample: "[{ ... }]"
-
 """
+import time
+import shlex
+
+def get_response(data, module):
+    try:
+        json = module.from_json(data)
+    except ValueError:
+        json = None
+    return dict(data=data, json=json)
+
+def get_value(data, key):
+    for k in key.split('.'):
+        data = data.get(k)
+    return data
+
+def eq(this, other):
+    return this == other
+
+def ne(this, other):
+    return this != other
+
+def gt(this, other):
+    return this > other
+
+def ge(this, other):
+    return this >= other
+
+def lt(this, other):
+    return this < other
+
+def le(this, other):
+    return this <= other
+
+def contains(this, other):
+    return this in other
+
+def to_re(expression):
+    expression = '^{}$'.format(expression)
+    return re.compile(expression)
+
+def match_re(regexp, values):
+    for value in values:
+        if regexp.match(value):
+            return True
+
+OPERATORS = {
+    eq: ['eq', '=='],
+    ne: ['ne', '!='],
+    gt: ['gt', '>'],
+    ge: ['ge', '>='],
+    lt: ['lt', '<'],
+    le: ['le', '<='],
+    contains: ['contains', 'in']
+}
 
 def main():
     spec = dict(
         command=dict(),
-        commands=dict(type='list'),
-        encoding=dict(default='json', choices=['json', 'text'])
+        waitfor=dict(),
+        retries=dict(default=10, type='int'),
+        interval=dict(default=1, type='int')
     )
-    mutually_exclusive = [('command','commands')]
-    required_one_of = [('command', 'commands')]
 
-    module = eapi_module(argument_spec=spec,
-                         mutually_exclusive=mutually_exclusive,
-                         required_one_of=required_one_of,
-                         supports_check_mode=True)
+    module = get_module(argument_spec=spec,
+                        supports_check_mode=True)
 
-    commands = module.params['commands'] or module.params['command']
-    encoding = module.params['encoding']
+    command = module.params['command']
+    waitfor = module.params['waitfor']
+    retries = module.params['retries']
+    interval = module.params['interval']
 
-    response, headers = eapi_command(module, commands, encoding)
+    if waitfor:
+        key, operator, value = shlex.split(waitfor)
 
-    result = dict(changed=False, result=response)
+        if value in BOOLEANS_TRUE:
+            value = True
+        elif value in BOOLEANS_FALSE:
+            value = False
+
+        if key[0] == 'r':
+            match = re.match(r'^r"(.*)"$')
+            if match:
+                regexp = to_re(match.group(1))
+
+        for func, operators in OPERATORS.items():
+            if operator in operators:
+                break
+        else:
+            module.fail_json(msg='unknown operator')
+
+        count = 0
+        while count != retries:
+            data = module.execute(command)
+            response = get_response(data[0], module)
+
+            curval = get_value(response.get('json'), key)
+            if func(curval, value):
+                break
+
+            time.sleep(interval)
+            count += 1
+        else:
+            module.fail_json(msg='timeout waiting for value')
+
+    result = dict(changed=False)
     return module.exit_json(**result)
 
 
 from ansible.module_utils.basic import *
 from ansible.module_utils.urls import *
-from ansible.module_utils.eapi import *
-
+from ansible.module_utils.shell import *
+from ansible.module_utils.netcfg import *
+from ansible.module_utils.eos import *
 if __name__ == '__main__':
-    main()
+        main()
+
