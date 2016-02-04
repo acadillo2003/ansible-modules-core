4e8d2426e2cb24c32d4be027f6b329d4fe69d73e
chouseknecht <chouseknecht@ansible.com>
2016-02-04 11:23:48 -0500

    Change parameter 'commands' to 'lines' in config moudule. Added suggested docs example.

--- network/nxos/nxos_config.py
+++ network/nxos/nxos_config.py
@@ -30,7 +30,7 @@ description:
     transports.
 extends_documentation_fragment: nxos
 options:
-  commands:
+  lines:
     description:
       - The ordered set of commands that should be configured in the
         section.  The commands must be the exact same commands as found
@@ -109,11 +109,11 @@ options:
 
 EXAMPLES = """
 - nxos_config:
-    commands: ['hostname {{ inventory_hostname }}']
+    lines: ['hostname {{ inventory_hostname }}']
     force: yes
 
 - nxos_config:
-    commands:
+    lines:
       - 10 permit ip 1.1.1.1/32 any log
       - 20 permit ip 2.2.2.2/32 any log
       - 30 permit ip 3.3.3.3/32 any log
@@ -124,7 +124,7 @@ EXAMPLES = """
     match: exact
 
 - nxos_config:
-    commands:
+    lines:
       - 10 permit ip 1.1.1.1/32 any log
       - 20 permit ip 2.2.2.2/32 any log
       - 30 permit ip 3.3.3.3/32 any log
@@ -133,11 +133,17 @@ EXAMPLES = """
     before: ['no ip access-list test']
     replace: block
 
+- nxos_config:
+    lines: "{{lookup('file', 'datcenter1.txt'}}"
+    parents: ['ip access-list test']
+    before: ['no ip access-list test']
+    replace: block
+
 """
 
 RETURN = """
 
-commands:
+lines:
   description: The set of commands that will be pushed to the remote device
   returned: always
   type: list
@@ -145,7 +151,7 @@ commands:
 
 response:
   description: The set of responses from issuing the commands on the device
-  retured: always
+  returned: always
   type: list
   sample: ['...', '...']
 
@@ -160,14 +166,14 @@ def get_config(module):
     return config
 
 
-def build_candidate(commands, parents, config, strategy):
+def build_candidate(lines, parents, config, strategy):
     candidate = list()
 
     if strategy == 'strict':
-        if len(commands) != len(config):
-            candidate = list(commands)
+        if len(lines) != len(config):
+            candidate = list(lines)
         else:
-            for index, cmd in enumerate(commands):
+            for index, cmd in enumerate(lines):
                 try:
                     if cmd != config[index]:
                         candidate.append(cmd)
@@ -175,16 +181,16 @@ def build_candidate(commands, parents, config, strategy):
                     candidate.append(cmd)
 
     elif strategy == 'exact':
-        if len(commands) != len(config):
-            candidate = list(commands)
+        if len(lines) != len(config):
+            candidate = list(lines)
         else:
-            for cmd, cfg in itertools.izip(commands, config):
+            for cmd, cfg in itertools.izip(lines, config):
                 if cmd != cfg:
-                    candidate = list(commands)
+                    candidate = list(lines)
                     break
 
     else:
-        for cmd in commands:
+        for cmd in lines:
             if cmd not in config:
                 candidate.append(cmd)
 
@@ -194,7 +200,7 @@ def build_candidate(commands, parents, config, strategy):
 def main():
 
     argument_spec = dict(
-        commands=dict(required=True, type='list'),
+        lines=dict(aliases=['commands'], required=True, type='list'),
         parents=dict(type='list'),
         before=dict(type='list'),
         after=dict(type='list'),
@@ -205,9 +211,9 @@ def main():
     )
 
     module = get_module(argument_spec=argument_spec,
-                         supports_check_mode=True)
+                        supports_check_mode=True)
 
-    commands = module.params['commands']
+    lines = module.params['lines']
     parents = module.params['parents'] or list()
 
     before = module.params['before']
@@ -235,14 +241,14 @@ def main():
 
     result = dict(changed=False)
 
-    candidate = build_candidate(commands, parents, children, match)
+    candidate = build_candidate(lines, parents, children, match)
 
     if candidate:
         if replace == 'line':
             candidate[:0] = parents
         else:
             candidate = list(parents)
-            candidate.extend(commands)
+            candidate.extend(lines)
 
         if before:
             candidate[:0] = before
@@ -255,7 +261,7 @@ def main():
             result['response'] = response
         result['changed'] = True
 
-    result['commands'] = candidate
+    result['lines'] = candidate
     return module.exit_json(**result)
 
 from ansible.module_utils.basic import *
